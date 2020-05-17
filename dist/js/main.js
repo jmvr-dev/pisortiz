47f7ee7afaebd2df9baf7d044d838f8e67d478b3
Manuel Velázquez <jmvr.dev@gmail.com>
2020-05-17 08:58:13 -0500

    Agrega galerías (móvil)

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,35 @@
+var mySwiper = new Swiper('#swiper-grafico', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
 
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+});
+
+var mySwiper = new Swiper('#swiper-diseno', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+});
+
+var mySwiper = new Swiper('#swiper-display', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+});
