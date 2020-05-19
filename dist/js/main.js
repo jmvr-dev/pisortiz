360e174c3e36685cf993267b96e40776806e1812
Manuel Velázquez <jmvr.dev@gmail.com>
2020-05-19 16:49:13 -0500

    Cambia glide.js a tiny-slider.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,35 @@
-new Glide('#glider-grafico').mount();
-new Glide('#glider-diseno').mount();
-new Glide('#glider-display').mount();
+var slider = tns({
+	center: true,
+	container: '#slider1',
+	controls: false,
+	items: 1,
+	mouseDrag: true,
+	navPosition: 'bottom',
+	slideBy: 'page',
+	swipeAngle: false,
+	speed: 400,
+});
+
+var slider = tns({
+	center: true,
+	container: '#slider2',
+	controls: false,
+	items: 1,
+	mouseDrag: true,
+	navPosition: 'bottom',
+	slideBy: 'page',
+	swipeAngle: false,
+	speed: 400,
+});
+
+var slider = tns({
+	center: true,
+	container: '#slider3',
+	controls: false,
+	items: 1,
+	mouseDrag: true,
+	navPosition: 'bottom',
+	slideBy: 'page',
+	swipeAngle: false,
+	speed: 400,
+});
